1f833b5bc90e2964acf4d0927cd0d40da15861d7
Kamleshwar Pandey <k.kamleshwar@abym.in>
2023-01-12 18:02:33 +0530

    local changes

--- src/component/feature/Home/BeautyPackage/BeautyPackageBox.js
+++ src/component/feature/Home/BeautyPackage/BeautyPackageBox.js
@@ -19,7 +19,7 @@ const BeautyPackageBox = ({item}) => {
                 <p className='mb-1'>Eyebrow</p>
                 <p className='mb-1'>Upper Lip</p>
             </div>
-            <div className='my-3'><span className='text-black-0f0 text-base'><span className='rupee-sym'>₹</span>1500</span><span className='text-black-a8a ml-3 line-through rupee-sym'>₹2000</span></div>
+            <div className='my-3'><span className='text-black-0f0 text-base font-bold'><span className='rupee-sym'>₹</span>1500</span><span className='text-black-a8a ml-3 line-through rupee-sym'>₹2000</span></div>
             <button className='custom_button' onClick={()=> router.push("/booknow")}>Book Now</button>
         </div>
     </div>
