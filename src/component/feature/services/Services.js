b9a5626e72a84ececa470b0b797137304a3238ad
Kamleshwar Pandey <k.kamleshwar@abym.in>
2023-01-14 12:30:10 +0530

    change

--- src/component/feature/services/Services.js
+++ src/component/feature/services/Services.js
@@ -24,10 +24,10 @@ const Services = () => {
   return (
     <div className='pt-3'>
         <div className='container'>
-            <div className='grid md:grid-cols-3 gap-y-1 items-center justify-center text-center bg-gray-e8e bg-opacity-[0.18] overflow-hidden rounded-2xl'>
+            <div className='grid md:grid-cols-3 gap-y-1 items-center justify-center text-center bg-gray-e8e bg-opacity-[0.18] overflow-hidden rounded-3xl'>
             {TabData?.map((item, idx) => (
                 <div key={"tabs"+idx} className='relative font-semibold cursor-pointer' onClick={()=> setTab(item.val)}>
-                    <Image src={IMAGES.ServiceTab} alt="beauty" className="relative h-20 object-cover"/>
+                    <Image src={IMAGES.ServiceTab} alt="beauty" className="relative h-110 object-cover"/>
                     <div className={`flex items-center text-opacity-100 justify-center absolute top-0 left-0 bottom-0 right-0 text-base${tab === item.val ? " text-white bg-gradient opacity-75" : " text-black-f0f"}`}>{item.label}</div>
                 </div>
             ))}
